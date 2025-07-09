9dbccf6b5dc67eed953e8e5689600b275de0a4aa
jaiswalaman2020 <jaiswalaman2020@gmail.com>
2025-07-09 21:13:38 +0530

    fix build

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { ModeToggle } from "./ModeToggle";
 import { CodeIcon } from "lucide-react";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import DasboardBtn from "./DasboardBtn";
